1fbb033d9a9268c8a5eefa31f1cf4f4b78e89111
geet <geet@Geet.(none)>
2014-01-27 11:29:39 +0530

    Example implementation of shortest path algorithm

--- python/graph/find_shortest_path.py
+++ python/graph/find_shortest_path.py
@@ -1,20 +1,41 @@
+from collections import deque
 graph = { 'a' :  [ 'b' , 'c' , 'd' , 'e', 'f' ],
           'b' :  [ 'x' , 'y', 'z' , 'c' , 'd' , 'e' ],
           'c' :  [ 'x' , 'd' , 'l' ],
           'd' :  [ 'l' , 'm' ],
-          'l' : ['p'] 
+          'l' : ['p'],
+          'x' : [],
+          'y' : [],
+          'z' : [],
+          'p' : [],
+          'm' : [],
+          'e' : [],
+          'f' : []
 
         }
 
-def shortest_path(graph, start, end, path = [])
+def shortest_path(graph, start, end, path = []):
     if not graph.has_key(start):
         return None
     if start == end : 
         return path
     path.append(start)
-    paths = []
-    for node in graph['start']:
-        if node == start:
-            return path
-    
+    paths=deque([])
+    paths.append(path)
+    curpath = paths.popleft()
+    while not curpath is None:
+        for node in graph[curpath[len(curpath) -1]]:
+            if node == end:
+                curpath.append(node)
+                return curpath
+            if node not in curpath:
+                newpath = curpath[:]
+                newpath.append(node)
+                paths.append(newpath)
+        curpath = paths.popleft()    
     return None
+
+# Example run below
+x = shortest_path(graph, 'a' , 'p' , [])
+print x
+
