8ddd59648535a2a1007bbdf5f3668fc05a8cc404
Nipun Talukdar <ntalukdar@actiance.com>
2014-09-25 12:16:55 +0530

    Added upd client server examples in go

---
+++ go/udpclient.go
@@ -0,0 +1,23 @@
+package main
+import (
+    "fmt"
+    "net"
+    "bufio"
+)
+
+func main() {
+    p :=  make([]byte, 2048)
+    conn, err := net.Dial("udp", "127.0.0.1:1234")
+    if err != nil {
+        fmt.Printf("Some error %v", err)
+        return
+    }
+    fmt.Fprintf(conn, "Hi UDP Server, How are you doing?")
+    _, err = bufio.NewReader(conn).Read(p)
+    if err == nil {
+        fmt.Printf("%s\n", p)
+    } else {
+        fmt.Printf("Some error %v\n", err)
+    }
+    conn.Close()
+}
