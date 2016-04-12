eff5f6e8746c804f8f9fdfefeceb0cd5979ead48
Nipun Talukdar <Nipunmlist@gmail.com>
2016-04-12 09:12:27 +0530

    Added expiry check for tuples

--- go/pipeline/src/gopipe/acker.go
+++ go/pipeline/src/gopipe/acker.go
@@ -1,16 +1,20 @@
 package gopipe
 
 import (
+	"container/heap"
 	"math"
 	"sync"
 	"sync/atomic"
+	"time"
 )
 
 const max_parallel uint = 128
+const fail_indicator uint64 = math.MaxUint64
 
 type Tracker interface {
 	Fail(id string)
 	Ack(id string)
+	TimedOut(id string)
 }
 
 type ackvalue struct {
@@ -32,6 +36,8 @@ type LocalAcker struct {
 	next_id           *uint64
 	tracker           Tracker
 	input_chan        chan *ackvalue
+	timeout           int64
+	exheap            *expiryHeap
 }
 
 func NewAckValue(id uint64, value uint64) *ackvalue {
@@ -41,7 +47,7 @@ func NewAckValue(id uint64, value uint64) *ackvalue {
 	return val
 }
 
-func NewLocalAcker(parallel uint, tracker Tracker) *LocalAcker {
+func NewLocalAcker(parallel uint, tracker Tracker, timeout int64) *LocalAcker {
 	if parallel == 0 || parallel > max_parallel {
 		parallel = max_parallel
 	}
@@ -67,6 +73,8 @@ func NewLocalAcker(parallel uint, tracker Tracker) *LocalAcker {
 	lc.and_with = uint(math.Pow(2, float64(parallel))) - 1
 	lc.tracker = tracker
 	lc.input_chan = make(chan *ackvalue, 102400)
+	lc.timeout = timeout
+	lc.exheap = NewExpiryHeap()
 	return lc
 }
 
@@ -77,7 +85,15 @@ func (acker *LocalAcker) AddTracking(ids string) uint64 {
 	defer acker.locks[index].Unlock()
 	acker.currentTracks[index][id] = 0
 	acker.currentTrackIds[index][id] = ids
+	expval := new(expiryValue)
+	expval.id = id
+	expval.timestamp = time.Now().Unix() + acker.timeout
+	heap.Push(acker.exheap, expval)
 	return id
+}
+
+func (acker *LocalAcker) SignalFail(id uint64) {
+	acker.AddAck(id, fail_indicator)
 }
 
 func (acker *LocalAcker) AddAck(id uint64, val uint64) {
@@ -88,22 +104,48 @@ func (acker *LocalAcker) AddAck(id uint64, val uint64) {
 func (acker *LocalAcker) updateAck(id uint64, val uint64) {
 	index := (acker.and_with & uint(id)) % acker.numParallelTracks
 	acker.locks[index].Lock()
-	defer acker.locks[index].Unlock()
 
 	cval, ok := acker.currentTracks[index][id]
 	if !ok {
+		acker.locks[index].Unlock()
 		return
 	}
 
-	cval ^= val
-	if cval != 0 {
-		acker.currentTracks[index][id] = cval
-	} else {
+	if val == fail_indicator {
 		delete(acker.currentTracks[index], id)
 		ids := acker.currentTrackIds[index][id]
 		delete(acker.currentTrackIds[index], id)
-		acker.tracker.Ack(ids)
+		acker.locks[index].Unlock()
+		acker.tracker.Fail(ids)
+	} else {
+		cval ^= val
+		if cval != 0 {
+			acker.currentTracks[index][id] = cval
+			acker.locks[index].Unlock()
+		} else {
+			delete(acker.currentTracks[index], id)
+			ids := acker.currentTrackIds[index][id]
+			delete(acker.currentTrackIds[index], id)
+			acker.locks[index].Unlock()
+			acker.tracker.Ack(ids)
+		}
 	}
+}
+
+func (acker *LocalAcker) handleTimeOut(id uint64) {
+	index := (acker.and_with & uint(id)) % acker.numParallelTracks
+	acker.locks[index].Lock()
+
+	_, ok := acker.currentTracks[index][id]
+	if !ok {
+		acker.locks[index].Unlock()
+		return
+	}
+	delete(acker.currentTracks[index], id)
+	ids := acker.currentTrackIds[index][id]
+	delete(acker.currentTrackIds[index], id)
+	acker.locks[index].Unlock()
+	acker.tracker.TimedOut(ids)
 }
 
 func (acker *LocalAcker) Start() {
@@ -111,6 +153,7 @@ func (acker *LocalAcker) Start() {
 	for ; num > 0; num-- {
 		go runAcker(acker)
 	}
+	go expiryChecker(acker)
 }
 
 func runAcker(acker *LocalAcker) {
@@ -118,4 +161,83 @@ func runAcker(acker *LocalAcker) {
 		aval := <-acker.input_chan
 		acker.updateAck(aval.id, aval.val)
 	}
+}
+
+func expiryChecker(acker *LocalAcker) {
+	time.Sleep(1 * time.Second)
+	for {
+		expval, ret := acker.exheap.popIfLess(time.Now().Unix())
+		if !ret {
+			time.Sleep(1 * time.Second)
+			continue
+		}
+		acker.handleTimeOut(expval.id)
+	}
+}
+
+type expiryValue struct {
+	timestamp int64
+	id        uint64
+}
+
+type expiryHeap struct {
+	expvals []*expiryValue
+	lock    *sync.Mutex
+}
+
+func NewExpiryHeap() *expiryHeap {
+	exh := new(expiryHeap)
+	exh.lock = new(sync.Mutex)
+	return exh
+}
+
+func (exh *expiryHeap) Len() int {
+	exh.lock.Lock()
+	defer exh.lock.Unlock()
+	return len(exh.expvals)
+}
+
+func (exh *expiryHeap) Less(i, j int) bool {
+	exh.lock.Lock()
+	defer exh.lock.Unlock()
+	return exh.expvals[i].timestamp < exh.expvals[j].timestamp
+}
+
+func (exh *expiryHeap) Swap(i, j int) {
+	exh.lock.Lock()
+	defer exh.lock.Unlock()
+	exh.expvals[i], exh.expvals[j] = exh.expvals[j], exh.expvals[i]
+}
+
+func (exh *expiryHeap) Push(x interface{}) {
+	exh.lock.Lock()
+	defer exh.lock.Unlock()
+	exh.expvals = append(exh.expvals, x.(*expiryValue))
+}
+
+func (exh *expiryHeap) Pop() interface{} {
+	exh.lock.Lock()
+	defer exh.lock.Unlock()
+	vals := exh.expvals
+	n := len(vals)
+	if n == 0 {
+		return nil
+	}
+	x := exh.expvals[n-1]
+	exh.expvals = exh.expvals[0 : n-1]
+	return x
+}
+
+func (exh *expiryHeap) popIfLess(t int64) (*expiryValue, bool) {
+	if exh.Len() == 0 {
+		return nil, false
+	}
+	exh.lock.Lock()
+	l := len(exh.expvals)
+	x := exh.expvals[l-1]
+	exh.lock.Unlock()
+	if x.timestamp <= t {
+		return heap.Pop(exh).(*expiryValue), true
+	}
+	return nil, false
 }
--- go/pipeline/src/gopipe/acker_test.go
+++ go/pipeline/src/gopipe/acker_test.go
@@ -17,9 +17,13 @@ func (tr *mytracker) Fail(id string) {
 	fmt.Printf("Failure for %s\n", id)
 }
 
+func (tr *mytracker) TimedOut(id string) {
+	fmt.Printf("TimeOut for %s\n", id)
+}
+
 func TestAcker(t *testing.T) {
 	tr := new(mytracker)
-	acker := NewLocalAcker(8, tr)
+	acker := NewLocalAcker(8, tr, 4)
 	acker.Start()
 	id := acker.AddTracking("hello")
 	fmt.Printf("Id returned by acker %d\n", id)
@@ -27,5 +31,12 @@ func TestAcker(t *testing.T) {
 	acker.AddAck(id, 200)
 	acker.AddAck(id, 200)
 	acker.AddAck(id, 100)
-	time.Sleep(2 * time.Second)
+	id = acker.AddTracking("hi")
+	acker.AddAck(id, 100)
+	acker.AddAck(id, 200)
+	acker.AddAck(id, 200)
+	acker.SignalFail(id)
+	id = acker.AddTracking("hey")
+	time.Sleep(10 * time.Second)
+
 }
