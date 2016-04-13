20063c310ae536b0178e7a3a685fd275d0d4acd8
Nipun Talukdar <Nipunmlist@gmail.com>
2016-04-13 13:41:13 +0530

    Changed an example

--- go/log/logrus.go
+++ go/log/logrus.go
@@ -4,31 +4,62 @@ import (
 	"bytes"
 	"fmt"
 	"github.com/Sirupsen/logrus"
+	"io/ioutil"
+	"os"
+	"syscall"
 	"time"
 )
 
 type MyIoWriter struct {
-	buffer *bytes.Buffer
+	outfile string
+	file    *os.File
+	buffer  *bytes.Buffer
 }
 
 func (myio *MyIoWriter) Write(p []byte) (n int, err error) {
 	n, err = myio.buffer.Write(p)
-	if err != nil {
+	if err != nil || n != len(p) {
 		panic("Write error")
 	}
-	if myio.buffer.Len() > 1280 {
-		fmt.Printf("%s\n", myio.buffer.Bytes())
+	n1 := myio.buffer.Len()
+	if n1 > 1280 {
+		n2, err := myio.file.Write(myio.buffer.Bytes())
+		if err != nil || n1 != n2 {
+			panic("Write error")
+		}
 		myio.buffer.Reset()
 	}
 
 	return n, err
 }
 
-func NewMyIoWriter() *MyIoWriter {
+func NewMyIoWriter(outfile string) *MyIoWriter {
 	buf := make([]byte, 2560)
 	buffer := bytes.NewBuffer(buf)
 	buffer.Reset()
-	return &MyIoWriter{buffer}
+	file, err := os.OpenFile(outfile, os.O_WRONLY|os.O_CREATE|os.O_APPEND, 0644)
+	if err != nil {
+		fmt.Printf("Failed to open file:%s, err:%v\n", file, err)
+		os.Exit(1)
+	}
+	fd := file.Fd()
+	if syscall.Flock(int(fd), syscall.LOCK_EX|syscall.LOCK_NB) != nil {
+		file.Close()
+		fmt.Printf("Unable to lock log file\n")
+		os.Exit(1)
+	}
+
+	files, err := ioutil.ReadDir("/home/geet/work/NipunTalukdarExamples/go")
+	if err != nil {
+		fmt.Printf("Unable to read the dir\n")
+		os.Exit(1)
+	}
+
+	for _, fino := range files {
+		fmt.Printf("File %s\n", fino.Name())
+	}
+
+	return &MyIoWriter{outfile, file, buffer}
 }
 
 func main() {
@@ -36,10 +67,10 @@ func main() {
 	log.Formatter = &logrus.TextFormatter{DisableColors: true,
 		TimestampFormat: "2006-01-02T15:04:05"}
 	log.Level = logrus.DebugLevel
-	mywriter := NewMyIoWriter()
+	mywriter := NewMyIoWriter("test.txt")
 	log.Out = mywriter
 	for {
 		log.Debug("Hello world, you are so beautiful")
-		time.Sleep(1 * time.Second)
+		time.Sleep(250 * time.Millisecond)
 	}
 }
