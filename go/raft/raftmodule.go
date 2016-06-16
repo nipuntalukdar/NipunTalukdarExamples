a797ef74955f8be9dc3859fb2d6165040826ae7a
Nipun Talukdar <Nipunmlist@gmail.com>
2016-06-16 17:57:28 +0530

    Expanded the example

--- go/raft/raftmodule.go
+++ go/raft/raftmodule.go
@@ -1,38 +1,121 @@
 package main
 
 import (
+	"bufio"
+	"bytes"
+	"encoding/gob"
+	"errors"
 	"fmt"
 	"github.com/hashicorp/raft"
 	"github.com/hashicorp/raft-boltdb"
 	"io"
 	"os"
+	"strings"
+	"sync"
 	"time"
 )
 
 type MyFsm struct {
+	mutex *sync.Mutex
+	data  map[string]string
+}
+
+func NewMyFsm() *MyFsm {
+	mutex := &sync.Mutex{}
+	data := make(map[string]string)
+	return &MyFsm{mutex: mutex, data: data}
 }
 
 type MyFsmSnapshot struct {
+	mfsm *MyFsm
+}
+
+func NewMyFsmSnapshot(fsm *MyFsm) *MyFsmSnapshot {
+	return &MyFsmSnapshot{fsm}
+}
+
+func (mfsm *MyFsm) Get(key string) (string, error) {
+	mfsm.mutex.Lock()
+	defer mfsm.mutex.Unlock()
+	val, ok := mfsm.data[key]
+	if ok {
+		return val, nil
+	} else {
+		return "", errors.New("Key not found")
+	}
+
 }
 
 func (mfsm *MyFsm) Apply(log *raft.Log) interface{} {
+	mfsm.mutex.Lock()
+	defer mfsm.mutex.Unlock()
+	ds := string(log.Data)
+	kvs := strings.SplitN(ds, ":", 2)
+	if len(kvs) != 2 {
+		return false
+	}
+	mfsm.data[kvs[0]] = kvs[1]
 	return true
 }
 
 func (mfsm *MyFsm) Snapshot() (raft.FSMSnapshot, error) {
-	return &MyFsmSnapshot{}, nil
+	fmt.Printf("Do snapshot..................\n")
+	return NewMyFsmSnapshot(mfsm), nil
 }
 
 func (mfsm *MyFsm) Restore(inp io.ReadCloser) error {
-	inp.Close()
+	defer inp.Close()
+	fmt.Printf("Restore......................\n")
+	mfsm.mutex.Lock()
+	defer mfsm.mutex.Unlock()
+	var buffer bytes.Buffer
+	readdata := make([]byte, 1024)
+	for {
+		n, err := inp.Read(readdata)
+		if err != nil {
+			panic(err)
+		}
+		if n < 1024 {
+			if n > 0 {
+				lastbytes := make([]byte, n)
+				copy(readdata, lastbytes)
+				buffer.Write(lastbytes)
+			}
+			break
+		} else {
+			buffer.Write(readdata)
+		}
+	}
+	dec := gob.NewDecoder(&buffer)
+	err := dec.Decode(&mfsm.data)
+	errorOnExit(err)
+	return nil
+}
+
+func (mfsm *MyFsm) Persist(sink raft.SnapshotSink) error {
+	mfsm.mutex.Lock()
+	defer mfsm.mutex.Unlock()
+
+	var buffer bytes.Buffer
+	enc := gob.NewEncoder(&buffer)
+	enc.Encode(mfsm.data)
+	n, err := sink.Write(buffer.Bytes())
+	if err != nil {
+		fmt.Printf("Error in snapshotting %s\n", err)
+		return err
+	}
+	fmt.Printf("Snapshotted %d bytes", n)
 	return nil
 }
 
 func (mfsmsh *MyFsmSnapshot) Persist(sink raft.SnapshotSink) error {
+	fmt.Printf("Persist...................\n")
+	mfsmsh.mfsm.Persist(sink)
 	return nil
 }
 
 func (mfsmsh *MyFsmSnapshot) Release() {
+	fmt.Printf("Released\n")
 }
 
 func errorOnExit(err error) {
@@ -42,12 +125,18 @@ func errorOnExit(err error) {
 	}
 }
 
-func main() {
-	var syr *raft.Raft = nil
-	fmt.Printf("%v\n", syr)
-	sstore, err := raftboltdb.NewBoltStore("/tmp/stablestore")
-	fmt.Printf("%v\n", sstore)
+func raftFutureErrorCheck(future raft.ApplyFuture) {
+	if err := future.Error(); err != nil {
+		fmt.Printf("Apply Error 1: %v\n", err)
+	}
 
+	if !future.Response().(bool) {
+		fmt.Printf("Apply Error 2:\n")
+	}
+}
+
+func main() {
+	sstore, err := raftboltdb.NewBoltStore("/tmp/stablestore")
 	if err != nil {
 		fmt.Printf("%v", err)
 		os.Exit(1)
@@ -58,7 +147,6 @@ func main() {
 		fmt.Printf("Failed to create logstore")
 		os.Exit(1)
 	}
-	fmt.Printf("%v\n", logstore)
 
 	snaps, err := raft.NewFileSnapshotStoreWithLogger("/tmp/snapshots", 3, nil)
 	errorOnExit(err)
@@ -67,10 +155,57 @@ func main() {
 	peerstore := raft.NewJSONPeers("/tmp/peers", transport)
 	conf := raft.DefaultConfig()
 	conf.EnableSingleNode = true
-	raftmod, err := raft.NewRaft(conf, &MyFsm{}, logstore, sstore,
+	conf.SnapshotThreshold = 40
+	conf.SnapshotInterval = 10 * time.Second
+	fsm := NewMyFsm()
+	raftmod, err := raft.NewRaft(conf, fsm, logstore, sstore,
 		snaps, peerstore, transport)
-	fmt.Printf("%v\n", raftmod)
 	time.Sleep(2 * time.Second)
 	fmt.Printf("Leader is %v\n", raftmod.Leader())
-	time.Sleep(200000 * time.Second)
+	future := raftmod.Apply([]byte("hello:value"), 0)
+	raftFutureErrorCheck(future)
+	i := 0
+	for ; i < 100; i++ {
+		time.Sleep(2 * time.Millisecond)
+		future := raftmod.Apply([]byte(fmt.Sprintf("key%d:value%d", i, i)), 0)
+		raftFutureErrorCheck(future)
+	}
+	fmt.Printf("Do some fun\n")
+
+	reader := bufio.NewReader(os.Stdin)
+	for {
+		fmt.Printf("Enter 1 to put, 2 to get, 3 to quit: ")
+		text, _ := reader.ReadString('\n')
+		text = strings.Trim(text, "\n")
+		if text == "3" {
+			os.Exit(0)
+		} else if text == "1" {
+			fmt.Printf("Key: ")
+			key, _ := reader.ReadString('\n')
+			key = strings.Trim(key, "\n\b \t\b")
+			if key == "" {
+				fmt.Printf("Empty key, continuing")
+				continue
+			}
+			fmt.Printf("Value: ")
+			value, _ := reader.ReadString('\n')
+			value = strings.Trim(value, "\n")
+			raftmod.Apply([]byte(fmt.Sprintf("%s:%s", key, value)), 0)
+		} else if text == "2" {
+			fmt.Printf("Key: ")
+			key, _ := reader.ReadString('\n')
+			key = strings.Trim(key, "\n\b \t\b")
+			if key == "" {
+				fmt.Printf("Empty key, continuing")
+				continue
+			}
+			val, err := fsm.Get(fmt.Sprintf(key))
+			if err != nil {
+				fmt.Printf("Failed to get %s\n", key)
+			} else {
+				fmt.Printf("The value for key:%s is %s\n", key, val)
+			}
+		}
+	}
+
 }
