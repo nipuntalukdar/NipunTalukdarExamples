b02a0744a650174b724a868eb2ba49f0ce2487c3
Nipun Talukdar <Nipunmlist@gmail.com>
2016-08-21 12:37:14 +0530

    Added description for algorithm

--- python/misc/print_permutations.py
+++ python/misc/print_permutations.py
@@ -1,6 +1,27 @@
 # Below function demonstrates how we can print the permutations of the letters
 # in a word
 
+###############################################################################
+# HOW it works?
+# it starts with the "smallest" possible word and successively prints the next
+# bigger word. For example, the smallest possible word from the chars in 
+# word 'axprq' is 'apqrx' and then the next bigger word is 'apqxr' and so on.
+# It stops after printing the "biggest" word which is 'xrqpa'.
+#
+# The advantage of this algorithm is that we don't print any duplicate words.
+#
+#
+# Given a word(current permutation), how do we find the next permutation?
+# ------------------------------ 
+# Start from last char and successively compare a char to the character in its
+# left. If the left character is smaller than the current character (say the 
+# position of the left character is exchange position), exchange the left 
+# charecter with a charecter to its right which is bigger than it and
+# nearest to it. When the exchange happens, sort the character array to the 
+# right of the exchange position.
+# Repeat the above process until the iteration when no exchange of characters
+# happens (i.e. it reaches the "biggest" word)
+
 def print_permute(word):
     a = sorted(word)
     l = len(word)
@@ -8,7 +29,6 @@ def print_permute(word):
     while exchanged:
         print ''.join(a)
         i = l - 1
-        smallest = i
         exchanged = False
         while i != 0:
             if a[i] <= a[i - 1]:
@@ -33,3 +53,7 @@ print('.............................')
 print('.............................')
 print ('Printing permutations of abcdd')
 print_permute('abcdd')
+print('.............................')
+print('.............................')
+print ('Printing permutations of axprq')
+print_permute('axprq')
