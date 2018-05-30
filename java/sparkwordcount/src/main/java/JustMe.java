356bb00b4f8575e6bb129ad16f9c3b7b7b8e5614
Nipun Talukdar <nipun.talukdar@colortokens.com>
2018-05-30 08:43:18 +0530

    Added few Spark examples

---
+++ java/sparkwordcount/src/main/java/JustMe.java
@@ -0,0 +1,9 @@
+
+public class JustMe {
+
+	public static void main(String[] args) {
+		System.out.println("Hello .......");
+
+	}
+
+}
---
+++ java/sparkwordcount/src/main/java/SparkWordCountDirectory.java
@@ -0,0 +1,44 @@
+import java.util.Arrays;
+import java.util.List;
+import java.util.regex.Pattern;
+import org.apache.spark.api.java.JavaPairRDD;
+import org.apache.spark.api.java.JavaRDD;
+import org.apache.spark.sql.SparkSession;
+import scala.Tuple2;
+
+
+public class SparkWordCountDirectory {
+
+
+
+	private static final Pattern SPACE = Pattern.compile(" ");
+
+	public static void main(String[] args) throws Exception {
+
+		System.out.println("Hello ");
+		if (args.length < 1) {
+			System.err.println("Usage: JavaWordCount <file>");
+			System.exit(1);
+		}
+
+		SparkSession spark = SparkSession.builder().appName("JavaWordCount").getOrCreate();
+
+		JavaRDD<String> lines = spark.read().textFile(args[0]).javaRDD();
+		JavaRDD<String> words = lines.flatMap(s -> Arrays.asList(SPACE.split(s)).iterator());
+
+		Thread.sleep(30000);
+		
+		JavaPairRDD<String, Integer> ones = words.mapToPair(s -> new Tuple2<>(s, 1));
+
+		JavaPairRDD<String, Integer> counts = ones.reduceByKey((i1, i2) -> i1 + i2);
+
+		List<Tuple2<String, Integer>> output = counts.collect();
+		for (Tuple2<?, ?> tuple : output) {
+
+			System.out.println("Here I am " + tuple._1() + ": " + tuple._2());
+
+		}
+		spark.stop();
+	}
+
+}
