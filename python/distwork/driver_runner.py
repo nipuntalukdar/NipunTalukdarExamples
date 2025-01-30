3f4ff73b9eeec1042b6d8e4ff3c7f0ed38ab5cce
Nipun Talukdar <nipun.talukdar@gmail.com>
2025-01-30 06:53:50 +0100

    Test change

--- python/distwork/driver_runner.py
+++ python/distwork/driver_runner.py
@@ -8,6 +8,6 @@ def my_fun(x:int =1 , y:int =2):
   return f"{ x + y }"
 
 rs = RedisStream()
-for i in range(2):
+for i in range(100):
   work = DumpLoad.dumpfn(my_fun, *(randint(1,100), randint(2,300)), **{})
   rs.enqueue_work({'work' : work})
